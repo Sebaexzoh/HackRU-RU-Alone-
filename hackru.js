ee4df4f5cb58fe1481e05eb7e2c496f6af9b2a0d
Sebaexzoh <99298164+Sebaexzoh@users.noreply.github.com>
2023-02-26 16:57:06 -0500

    Add files via upload

---
+++ hackru.js
@@ -0,0 +1,14 @@
+function scaleup(obj, size){
+    obj.style.fontSize = size;
+}
+
+function scaleup2(obj, size){
+    obj.style.fontSize = size;
+    obj.style.transform = "scale(1.05)";
+}
+
+function scaledown(obj)
+{
+    obj.style.fontSize = "30px"
+    obj.style.transform= "scale(1)";
+}
